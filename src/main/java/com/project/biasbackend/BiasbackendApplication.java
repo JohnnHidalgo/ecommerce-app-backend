ab31509a5a4aacaca88acf4c6c8f9f9438255bf2
MauroCelis <mau.armata@gmail.com>
2020-09-25 16:21:19 -0400

    First commit

---
+++ src/main/java/com/project/biasbackend/BiasbackendApplication.java
@@ -0,0 +1,13 @@
+package com.project.biasbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BiasbackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BiasbackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/biasbackend/BiasbackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.biasbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BiasbackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
